c25b6425da10f2f4f844c60d8142ce2752bdd65b
Jeremy Eramian <jeremyleramian@outlook.com>
2015-10-04 09:04:06 -0700

    adding login

---
+++ template.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link type="text/css" rel="stylesheet" href="layout.css"/>
+        <link type="text/css" rel="stylesheet" href="menu.css"/>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
