80807e52e117099a0fb24df694275d52410c2b9e
Jeremy Eramian <jeremyleramian@outlook.com>
2015-11-04 14:50:30 -0800

    Hey it works!

--- pricingcalc.js
+++ pricingcalc.js
@@ -7,13 +7,19 @@ var pageNum = document.getElementById('pageNum').value;
 var store = document.getElementById('storeReq').accessKey;
 var user = document.getElementById('userReq').accessKey;
 
-if(pageNum = 3) {
+if(pageNum == 1) {
+	pagePrice = 50;
+}
+else if(pageNum == 2) {
+	pagePrice = 50;
+}
+else if(pageNum == 3) {
 	pagePrice = 150;
 }
-else if(pageNum = 4) {
+else if(pageNum == 4) {
 	pagePrice = 200; 
 }
-else if(pageNum = 5) {
+else if(pageNum == 5) {
 	pagePrice = 250;
 }
 else if(pageNum > 5) {
@@ -22,5 +28,28 @@ else if(pageNum > 5) {
 else {
 	alert('Something went wrong');
 }
-alert(pagePrice);
+
+if(store == 'yes'||'Yes'){
+	storePrice = 100;
+}
+else if(store == 'no'||'No') {
+	storePrice = 0;
+}
+else {
+	alert('Something went wrong');
+}
+
+if(user == 'yes'||'Yes') {
+	userPrice = 65;
+}
+else if(user == 'no'||'No') {
+	userPrice = 0;
+}
+else {
+	alert('Something went wrong');
+}
+
+var price = pagePrice + storePrice + userPrice;
+
+alert(price);
 }
