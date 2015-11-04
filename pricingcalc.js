9bf7fd91ae7226d60bd8077cca4377df773d04c9
Jeremy Eramian <Jeramian@users.noreply.github.com>
2015-11-04 11:47:13 -0800

    Update pricingcalc.js

--- pricingcalc.js
+++ pricingcalc.js
@@ -7,39 +7,19 @@ var pageNum = document.getElementById('pageNum').value;
 var store = document.getElementById('storeReq').accessKey;
 var user = document.getElementById('userReq').accessKey;
 
-if(pageNum === 3) {
+if(pageNum = 3) {
 	pagePrice = 150;
 }
-else if(pageNum === 4) {
-	pagePrice = 200;
+else if(pageNum = 4) {
+	pagePrice = 200; 
 }
-else if(pageNum === 5) {
+else if(pageNum = 5) {
 	pagePrice = 250;
 }
 else if(pageNum > 5) {
-	pagePrice = 400
+	pagePrice = 300;
 }
 else {
-	alert('Were sorry, something went wrong, please check that you filled in all required feilds or left them at their default setting');
-}
-
-if(store = 'yes' || 'Yes') {
-	storePrice = 150;
-}
-else if(store = 'no' || 'No') {
-	storePrice = 0;
-}
-else {
-	alert('Were sorry, something went wrong, please check that you filled in all required feilds or left them at their default setting');
-}
-
-if(user = 'yes' || 'Yes'){
-	userPrice = 50;
-}
-else if(user = 'no' || 'No') {
-	userPrice = 0;
-}
-else {
-	alert('Were sorry, something went wrong, please check that you filled in all required feilds or left them at their default setting');
+	alert('Did you enter a page numbber? Or leave it with the original value?')
 }
 }
