941fb364d2604450733f8d7e473d8e4492496704
Jeremy Eramian <jeremyleramian@outlook.com>
2015-11-04 17:00:58 -0800

    yaaaay this worked too

---
+++ login.js
@@ -0,0 +1,46 @@
+function userAuth() {
+	var idNumber = document.getElementById('idNumber').value;
+	
+	if(idNumber == 2518665) {
+		alert('Welcome back, Jeremy');
+		var btn = document.createElement("BUTTON");
+		var btnText = document.createTextNode("Continue");
+		btn.appendChild(btnText);
+		btn.setAttribute('id', 'continueButton');
+		btn.setAttribute('class', 'btn btn-default');
+		document.getElementById('loginField').appendChild(btn);
+		document.body.appendChild(btn);
+		document.getElementById('loginField').appendChild(btn);
+		btn.setAttribute('onclick', location.href='admin.html');
+	}
+	else if(idNumber == 2490318) {
+		alert('Welcome back, Tyler');
+		var btn = document.createElement("BUTTON");
+		var btnText = document.createTextNode("Continue");
+		btn.appendChild(btnText);
+		btn.setAttribute('id', 'continueButton');
+		btn.setAttribute('class', 'btn btn-default');
+		document.getElementById('loginField').appendChild(btn);
+		document.body.appendChild(btn);
+		document.getElementById('loginField').appendChild(btn);
+		btn.setAttribute('onclick', location.href='admin.html');
+	}
+	else if(idNumber == 2509987) {
+		alert('Welcome back, Mollee');
+		var btn = document.createElement("BUTTON");
+		var btnText = document.createTextNode("Continue");
+		btn.appendChild(btnText);
+		btn.setAttribute('id', 'continueButton');
+		btn.setAttribute('class', 'btn btn-default');
+		document.getElementById('loginField').appendChild(btn);
+		document.body.appendChild(btn);
+		document.getElementById('loginField').appendChild(btn);
+		btn.setAttribute('onclick', location.href='admin.html');
+	}
+	else if(idNumber == null) {
+		alert('Please enter your ID');
+	}
+	else {
+		alert('ID number not recognized');
+	}
+}
