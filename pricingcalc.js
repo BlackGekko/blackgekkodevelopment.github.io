a49fe0ac13ef4102a17c60ab87b33a95786316cc
Jeremy Eramian <jeremyleramian@outlook.com>
2015-11-11 11:09:17 -0800

    re-writing dcript

--- pricingcalc.js
+++ pricingcalc.js
@@ -1,55 +1,62 @@
 var pagePrice;
 var storePrice;
-var userPrice;
+var databasePrice;
+var finalPrice = pagePrice + storePrice + databasePrice;
 
 function priceCalc() {
-var pageNum = document.getElementById('pageNum').value;
-var store = document.getElementById('storeReq').accessKey;
-var user = document.getElementById('userReq').accessKey;
-
-if(pageNum == 1) {
-	pagePrice = 50;
-}
-else if(pageNum == 2) {
-	pagePrice = 50;
-}
-else if(pageNum == 3) {
-	pagePrice = 150;
-}
-else if(pageNum == 4) {
-	pagePrice = 200; 
-}
-else if(pageNum == 5) {
-	pagePrice = 250;
-}
-else if(pageNum > 5) {
-	pagePrice = 300;
-}
-else {
-	alert('Something went wrong');
-}
-
-if(store == 'yes'||'Yes'){
-	storePrice = 100;
-}
-else if(store == 'no'||'No') {
-	storePrice = 0;
-}
-else {
-	alert('Something went wrong');
-}
-
-if(user == 'yes'||'Yes') {
-	userPrice = 65;
-}
-else if(user == 'no'||'No') {
-	userPrice = 0;
-}
-else {
-	alert('Something went wrong');
-}
-
-var price = pagePrice + storePrice + userPrice;
-
-alert(price);
+	var price = document.createElement('p');
+	var node = document.createTextNode('The estimate of your site is: ' + storePrice);
+	price.appendChild(node);
+	var element = document.getElementById('finalPrice');
+	element.appendChild(price);
+	
+	var pageAmount = document.getElementById('page').value;
+	var storeOption = document.getElementById('store').value;
+	var databaseOption = document.getElementById('database').value;
+	event.preventDefault();
+	
+	//Page amount and price
+	if(pageAmount == 1) {
+		pagePrice = 50;
+	}
+	else if(pageAmount == 2) {
+		pagePrice = 60;
+	}
+	else if(pageAmount == 3) {
+		pagePrice = 100;
+	}
+	else if(pageAmount == 4) {
+		pagePrice = 150;
+	}
+	else if(pageAmount == 5) {
+		pagePrice == 200;
+	}
+	else if(pageAmount > 5) {
+		pagePrice = 300;
+	}
+	else {
+		alert('Something went wrong!')
+	}
+	
+	//Store option and price
+	if(storeOption == 1) {
+		storePrice = 100;
+	}
+	else if(storeOption == 2) {
+		storePrice = 0;
+	}
+	else {
+		alert("Something went wrong");
+	}
+	
+	//Database option and price
+	if(databaseOption == 1) {
+		databasePrice = 100;
+	}
+	else if(databaseOption == 2) {
+		databasePrice = 0;
+	}
+	else {
+		alert("Something went wrong");
+	}
 }
